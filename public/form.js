635e0b7e00bf6488fc8cc3d09de6d4eb5f76516c
Caleb Baird <caleb5baird@gmail.com>
2019-06-28 13:07:55 -0600

    adding new poll

--- public/form.js
+++ public/form.js
@@ -12,7 +12,7 @@ function viewPoll() {
 		var html = "<table>";
 		html += "<tr>" + "<th>Question</th>" + "<th>Start Date</th>" + "<th>End Date</th>" + "</tr>";
 		for (var i = 0; i < data.length; ++i) {
-			html += "<td>" + data[i].question + "</td>" + "<td>" data[i].start_date + "</td>" + "<td>" + data[i].end_date + "</td>";
+			html += "<td>" + data[i].question + "</td>" + "<td>" + data[i].start_date + "</td>" + "<td>" + data[i].end_date + "</td>";
 		}
 		html += "</table>";
 		document.getElementById("polls").innerHTML = html;
