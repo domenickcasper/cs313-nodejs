507c294a890da2a253727e84895e6180cfc652de
Caleb Baird <caleb5baird@gmail.com>
2019-06-28 12:03:42 -0600

    Added it

--- public/form.js
+++ public/form.js
@@ -1,8 +1,8 @@
 function getUser() {
 	$.get('/getUser', function(data, status) {
 		var html = "";
-		html += "<h1>" + data.name + "</h1>";
-		html += "<h2>" + data.username + "</h2>"
+		html += "<h1>" + data[0].name + "</h1>";
+		html += "<h2>" + data[0].username + "</h2>"
 		document.getElementById("users").innerHTML = html;
 	})	
 }
