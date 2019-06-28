2bd8570f94b31fa7dfae8b182982123d93f09724
Caleb Baird <caleb5baird@gmail.com>
2019-06-28 11:41:38 -0600

    Starting my project

---
+++ database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+id SERIAL PRIMARY KEY
+, name VARCHAR(100) NOT NULL
+, username VARCHAR(30)  NOT NULL UNIQUE
+, password VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE poll (
+id SERIAL PRIMARY KEY
+, question VARCHAR(5000) NOT NULL
+, start_date DATE NOT NULL
+, end_date DATE NOT NULL
+);
+
+CREATE TABLE input (
+id SERIAL PRIMARY KEY
+, poll_id INT REFERENCES poll(id)
+, input VARCHAR(5000) NOT NULL
+, count INT 
+, why VARCHAR(5000)
+);
+
