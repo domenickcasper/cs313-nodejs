b1976db6683e6c741c7f77d1579955905b0261b0
Caleb Baird <caleb5baird@gmail.com>
2019-07-03 22:04:24 -0600

    added

--- public/form.js
+++ public/form.js
@@ -30,7 +30,7 @@ function viewPoll() {
 function addPoll() {
 	var question = $("#question").val();
 	var end = $("#end").val();
-	var answer = $(".answer").val();
+	var answer[] = $(".answer").val();
 
 	$.post('/addPoll', {question: question, end: end, answer: answer}, function(data, status) {
 
