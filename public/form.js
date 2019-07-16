b331cc633b21d433a5811088bd6643eb3c22e677
Caleb Baird <caleb5baird@gmail.com>
2019-07-16 12:43:20 -0600

    Stuff

--- public/form.js
+++ public/form.js
@@ -45,7 +45,7 @@ function viewThePoll(id) {
 	})
 }
 
-/*function viewResults(id) {
+function viewResults(id) {
 	$.get('/viewResults?count=' + id, function(data, status) {
 		//$('#viewThePoll').hide();
 		var html = "" + data[0].count + "<br>";
@@ -58,7 +58,7 @@ function viewThePoll(id) {
 		
 	})
 }
-*/
+
 
 
 function submitPoll() {
