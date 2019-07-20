e1786527f44c70d59ebac757d471a132f9613081
Domenick Casper <domenicktcasper@gmail.com>
2019-07-20 09:44:14 -0600

    Stuff

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ express()
     	var ct = 0;
     	var ans = req.body.answer.split('-');
     	for (var i = 0; i < ans.length - 1; i++) {
-    		pool.query(sqli, [data[0].rows.id, ans[i]], function (err, data) {
+    		pool.query(sqli, [data.rows[0].id, ans[i]], function (err, data) {
     			if (ct == ans.length - 1) {
     				res.json({Message: "Added to the database"});
     			}
-    		}) 
+    		})
     		ct++;
     	}
     }) 
